0ca5bad832694d5909e75b8b7b4a56d997d37c7e
Charles Truscott Watters <119706319+disabledmathematician@users.noreply.github.com>
2024-03-07 12:03:10 +1100

    Add files via upload

--- rolls.py
+++ rolls.py
@@ -8,13 +8,20 @@ def rolls():
 	for x in range(100):
 		L.append(random.choice(d1) + random.choice(d2))
 	plt.figure(0, dpi=150, figsize=[15, 7])
-	plt.title("Charles Truscott")
-	plt.plot([x for x in range(len(L) // 4)], L[:len(L) // 4], color='purple', label='25 trials')
-	plt.plot([x for x in range(len(L) // 2)], L[:len(L) // 2], color='silver', label='50 trials')
+	plt.title("Descartes Plot for Trials of Sums of Die Rolls. Charles Truscott")
+	
 	plt.plot([x for x in range(len(L))], L[:len(L)], color='gold', label='100 trials')
+	plt.plot([x for x in range(len(L) // 2)], L[:len(L) // 2], color='silver', label='50 trials')
+	plt.plot([x for x in range(len(L) // 4)], L[:len(L) // 4], color='purple', label='25 trials')
+	
 	x = [len(L) // 4, len(L) // 2, len(L)]
 	y = [np.mean(L[:len(L) // 4]), np.mean(L[:len(L) // 2]), np.mean(L[:])]
 	plt.plot(x, y, color='pink', label="running avg")
 	plt.legend()
-	plt.savefig('100_trials.png')
+#	plt.show()
+	plt.savefig('all_trials.png')
+	plt.figure(1, dpi=150, figsize=[15, 7])
+	plt.title("Histogram for sum of two die rolls. Charles Truscott")
+	plt.hist(L, linewidth=0.5, edgecolor='black')
+	plt.savefig('all_hist.png')
 rolls()
